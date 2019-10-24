3c9211065b0627826b16a03dfcfcfeebe7b1bff1
GustavoFCoelho <gustavo.f.coelho09@gmail.com>
2019-10-24 11:32:35 -0300

    Primeiras Configurações, Define dominios e cria repositorios JPA

---
+++ src/main/java/gerenciamento/garagem/GEEstacionamentos/GeEstacionamentosApplication.java
@@ -0,0 +1,13 @@
+package gerenciamento.garagem.GEEstacionamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeEstacionamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(gerenciamento.garagem.GEEstacionamentos.GeEstacionamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gerenciamento/garagem/GEEstacionamentos/GeEstacionamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenciamento.garagem.GEEstacionamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeEstacionamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
